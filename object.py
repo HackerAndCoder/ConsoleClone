02ccad5cedba6edc5d62917500f9e33530465c59
TheCrazyModder <100743043+HackerAndCoder@users.noreply.github.com>
2023-07-14 17:42:43 -0600

    Added "Sprite" class

--- object.py
+++ object.py
@@ -3,3 +3,7 @@ import pygame, settings
 class Object:
     def __init__(self, texture : pygame.Surface):
         self.texture = pygame.transform.scale(texture, (settings.tile_size, settings.tile_size))
+
+class Sprite:
+    def __init__(self, texture : pygame.Surface):
+        self.texture = pygame.transform.scale(texture, (settings.tile_size, settings.tile_size))
